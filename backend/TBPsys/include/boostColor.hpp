9fb0ed67159fc55582c6142953a9e45d44754c78
Montag55 <lucas.huebner@uni-weimar.de>
2018-11-09 20:47:43 +0100

    added transferfunction interpretation, changes in BosstColor class design

--- backend/TBPsys/include/boostColor.hpp
+++ backend/TBPsys/include/boostColor.hpp
@@ -9,20 +9,20 @@
 
 class BoostColor : public Interpretation{
 public:
-  BoostColor(std::shared_ptr< Base > mother, int id, int type, float threshhold, std::vector<float> color);
+  BoostColor(std::shared_ptr< Base > mother, int id, int type, float threshhold, std::shared_ptr<std::vector<float>> color);
   ~BoostColor()                       override;
   int getTypenumber()                 override;
   int get_calculation_specification() override;
   void calc(int id, int start, int length, int sign, cv::Mat& result, float& factor, cv::Mat& fac_mat) override;
   void compute_frame(cv::Mat& result, cv::Mat& fac_mat, cv::Mat& current_frame, int sign);
-  void manipulate(float threshhold, std::vector<float> colors);
+  void manipulate(float threshhold, std::shared_ptr<std::vector<float>> colors);
 
 private:
   float m_threshhold;
-  std::vector<float> m_colors;
   int m_ptr_delta;
   cv::Point m_pnt_min;
   cv::Point m_pnt_max;
+  std::shared_ptr<std::vector<float>> m_colors;
 };
 
 #endif
